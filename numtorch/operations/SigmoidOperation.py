Fix backprop

numtorch/operations/SigmoidOperation.py
@@ -25,4 +25,4 @@ class SigmoidOperation(Operation):
         parent = self.ctx.meta["parents"][0]
         ones = self.ctx.tensor(np.ones_like(self.ctx.grad.data))
 
-        parent.backward(self.ctx.grad * (self.ctx * (ones - self.ctx)), self.ctx)
+        parent.backward(self.ctx.grad * (self.ctx * (ones - self.ctx)))

numtorch/operations/TanhOperation.py
@@ -26,6 +26,5 @@ class TanhOperation(Operation):
         ones = self.ctx.tensor(np.ones_like(self.ctx.grad.data))
 
         parent.backward(
-            self.ctx.grad * (ones - (self.ctx * self.ctx)),
-            self.ctx
+            self.ctx.grad * (ones - (self.ctx * self.ctx))
         )